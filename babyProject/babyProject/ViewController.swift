a3eadfaf535e0ccabef46c8858d56fb5336dd082
Otto linden <otto.linden@outlook.com>
2021-03-31 19:28:11 +0300

    Initial commit

---
+++ babyProject/babyProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  babyProject
+//
+//  Created by Otto Linden on 31.3.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
